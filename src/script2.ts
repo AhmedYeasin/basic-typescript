c075595f1310123041004183f2329b746aa275cd
AhmedYeasin <ahmetchatgami@gmail.com>
2026-04-29 23:34:06 +0600

    Keyof object in TypeScript

--- src/script2.ts
+++ src/script2.ts
@@ -16,3 +16,19 @@ const worker: Employee = {
 };
 
 console.log(worker)
+
+interface Car {
+  brand: string;
+  model: string;
+  year: number;
+}
+
+
+// Keyof object in TypeScript
+function getCarProperty(car: Car, key: keyof Car) {
+  return car[key];
+}
+
+const myCar: Car = { brand: "Toyota", model: "Corolla", year: 2022 };
+
+console.log(getCarProperty(myCar, "brand"));
